1ad3027075779104ce896f3b74862bb988c363b2
Ruslan_Kirsanov <74593778+Hornerer@users.noreply.github.com>
2021-11-10 22:06:16 +0300

    Delete 5.1.sql

--- 5.1.sql
+++
@@ -1,5 +0,0 @@
-select orders.onum, salespeople.sname, customers.cname, orders.odate, orders.amt
-from orders
-left outer join salespeople on orders.snum = salespeople.snum
-left outer join customers on orders.cnum = customers.cnum
-order by onum
